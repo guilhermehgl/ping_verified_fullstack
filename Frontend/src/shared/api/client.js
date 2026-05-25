e2e60ae922a0ac75d190fe6d8bf3011122b984dc
guilhermehgl <guilhermehgl07@gmail.com>
2026-05-25 16:33:32 -0300

    Mais ajustes de deploy

--- Frontend/src/shared/api/client.js
+++ Frontend/src/shared/api/client.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const devApiBaseUrl = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000'
+const devApiBaseUrl = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3004'
 
 export const apiClient = axios.create({
   baseURL: import.meta.env.DEV ? devApiBaseUrl : '',
